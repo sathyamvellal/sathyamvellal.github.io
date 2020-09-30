a46e3b66d7b13bef1ed9d5073d5328e102137bec
Sathyam Vellal <sathyam.vellal@gmail.com>
2020-09-30 10:26:51 -0700

    Initial commit for eleventy-port
    - Travis is broken here

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addShortcode("greet", function(entity) {
+        return entity + "!";
+    });
+
+    eleventyConfig.addPassthroughCopy({"source/_static" : "/"});
+
+    return {
+        pathPrefix: "/",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+        markdownTemplateEngine: "njk",
+        dir: {
+            input: "source",
+            output: "_site",
+            includes: "_includes",
+            layouts: "_layouts",
+            data: "_data"
+        },
+    };
+}
