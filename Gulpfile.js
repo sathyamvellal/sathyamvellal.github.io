1106ec1e7ee76b85bd592aa83b9a9e66f2ac8fd2
Sathyam Vellal <sathyam.vellal@gmail.com>
2020-12-07 23:18:18 -0800

    Reconcile Gulipfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,27 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass");
+var gulp = require("gulp");
+var exec = require('child_process').exec;
+var sass = require("gulp-sass");
 
-gulp.task("css", function() {
+var buildCss = () => {
     return gulp.src("./sass/*.scss")
             .pipe(sass({
                 outputStyle: "compressed"
             })
             .on("error", sass.logError))
             .pipe(gulp.dest("./source/_static/css/"));
-});
+};
+
+var buildEleventy = () => {
+    return exec('eleventy', (err, stdout, stderr) => {
+        if (err) {
+            throw err;
+        }
+
+        console.log(stdout);
+        console.error(stderr);
+    });
+}
+
+exports.buildCss = buildCss;
+exports.buildEleventy = buildEleventy;
+exports.default = gulp.series(buildCss, buildEleventy);
