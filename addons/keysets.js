aedebf558d7305f19855b6e486b5401424540c06
Sathyam Vellal <sathyam.vellal@gmail.com>
2021-01-02 08:28:01 -0800

    Refactor keysets addon

--- addons/keysets.js
+++ addons/keysets.js
@@ -1,24 +1,21 @@
 class KeySet {
     constructor(keyName) {
         this.keyName = keyName;
-        this.collection = this._collection();
     };
 
-    _collection() {
+    collection(collectionName) {
         var keyName = this.keyName;
-        return function(collectionName) {
-            return function(eleventyCollections) {
-                var array = new Array();
-                eleventyCollections.getAllSorted().forEach(function(item) {
-                    if (item.data[keyName] && item.data[keyName].includes(collectionName)) {
-                        array.push(item);
-                    }
-                });
+        return function(eleventyCollections) {
+            var array = new Array();
+            eleventyCollections.getAllSorted().forEach(function(item) {
+                if (item.data[keyName] && item.data[keyName].includes(collectionName)) {
+                    array.push(item);
+                }
+            });
 
-                return [...array];
-            }; // eleventyCollections
-        }; // collectionName
-    }; // _collection
+            return [...array];
+        }; // eleventyCollections
+    }; // collectionName
 };
 
 module.exports = KeySet;
